61fbf454d5f926e2109554c5d0cd206cad8c87bc
Maciej Debski <pain.floyd.maq@gmail.com>
2022-02-19 21:31:11 +0100

    Add model for workflows

---
+++ panderyx/workflows/models.py
@@ -0,0 +1,16 @@
+import uuid
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from panderyx.users.models import User
+
+
+class Workflow(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name=_("workflow name"))
+    date_created = models.DateTimeField(
+        auto_now_add=True, verbose_name=_("date of creation")
+    )
+    date_updated = models.DateTimeField(auto_now=True, verbose_name=_("last update"))
