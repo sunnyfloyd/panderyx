3316b83c0915a1dc16d3420e52e68ffa225aafdd
Maciej Debski <pain.floyd.maq@gmail.com>
2022-02-26 15:51:33 +0100

    Add Tool model schema and create initial migration

---
+++ panderyx/tools/models.py
@@ -0,0 +1,38 @@
+import uuid
+
+from django.core.validators import MinValueValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from panderyx.workflows.models import Workflow
+
+
+class Tool(models.Model):
+    workflow = models.ForeignKey(
+        Workflow, on_delete=models.CASCADE, related_name="tools"
+    )
+    name = models.CharField(max_length=100, verbose_name=_("tool name"), null=True)
+    x = models.IntegerField(
+        verbose_name=_("x coordinate"), validators=[MinValueValidator(0)], default=100
+    )
+    y = models.IntegerField(
+        verbose_name=_("y coordinate"), validators=[MinValueValidator(0)], default=100
+    )
+
+    class ToolType(models.TextChoices):
+        URL_INPUT = "url input", _("URL Input")
+        DATA_INPUT = "data input", _("Data Input")
+
+    input_type = models.CharField(choices=ToolType.choices, max_length=100)
+    inputs = models.ManyToManyField("self", symmetrical=False, related_name="outputs")
+    config = models.JSONField(null=True)
+    data = models.URLField(null=True)
+
+    class Meta:
+        ordering = ["workflow", "name"]
+
+    def __str__(self) -> str:
+        return f"{self.get_name()} in {self.workflow.name}"
+
+    def get_name(self):
+        return self.name or f"Tool ({self.id})"
