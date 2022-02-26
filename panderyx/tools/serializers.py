298c76fe61b2b03cb38318af766cb0cbff921941
Maciej Debski <pain.floyd.maq@gmail.com>
2022-02-26 16:51:28 +0100

    Add serializer for Tool model

---
+++ panderyx/tools/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from panderyx.tools.models import Tool
+
+
+class ToolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tool
+        fields = "__all__"
+        read_only_fields = [
+            "workflow",
+            "data",
+        ]
