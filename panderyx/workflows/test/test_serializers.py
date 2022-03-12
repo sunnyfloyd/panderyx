bc962d1f34484dd7df94395705b245d0d9a80f74
Maciej Debski <pain.floyd.maq@gmail.com>
2022-03-12 13:33:20 -0800

    Add workflow serializer tests

---
+++ panderyx/workflows/test/test_serializers.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from django.test import TestCase
+from django.forms.models import model_to_dict
+
+from panderyx.workflows.serializers import WorkflowSerializer
+from panderyx.workflows.test.factories import WorkflowFactory
+
+
+class TestWorkflowSerializer(TestCase):
+    def setUp(self):
+        self.workflow_data = model_to_dict(WorkflowFactory.build())
+
+    def test_serializer_with_empty_data(self):
+        serializer = WorkflowSerializer(data={})
+        assert serializer.is_valid() == False
+
+    def test_serializer_with_valid_data(self):
+        serializer = WorkflowSerializer(data=self.workflow_data)
+        assert serializer.is_valid() == True
