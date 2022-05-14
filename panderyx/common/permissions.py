38908e2c4deeead2e80341135c6b8114f952d3af
Maciej Debski <pain.floyd.maq@gmail.com>
2022-05-14 11:02:59 +0200

    Adjust docstring and comments for IsWorkflowOwnerOrAdmin

--- panderyx/common/permissions.py
+++ panderyx/common/permissions.py
@@ -4,14 +4,17 @@ from rest_framework import permissions
 class IsWorkflowOwnerOrAdmin(permissions.BasePermission):
     """
     Permission that allows only owners of an object or admins to view or edit it.
-    It takes into account
+
+    This check is designed to work with both Workflow and Tool objects.
     """
 
     def has_object_permission(self, request, view, obj):
+        # check for tool views
         if hasattr(obj, "workflow"):
             return obj.workflow.user == request.user or bool(
                 request.user and request.user.is_staff
             )
+        # check for workflow views
         else:
             return obj.user == request.user or bool(
                 request.user and request.user.is_staff
